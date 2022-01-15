597fcc15a874320c4d27f418e2611fe3627e162b
Axel06 <Anisvan@mail.ru>
2022-01-15 21:49:16 +0600

    added SMTP parametrs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,8 +74,8 @@ Rails.application.configure do
   :address  => 'smtp.sendgrid.net',
   :port     => '587',
   :authentication => :plain,
-  :user_name  => ENV['SENDGRID_USERNAME'],
-  :password   => ENV['SENDGRID_PASSWORD'],
+  :user_name  => ENV['app245170241@heroku.com'],
+  :password   => ENV['jp4q53jv1823'],
   :domain     => 'heroku.com',
   :enable_starttls_auto => true
   }
